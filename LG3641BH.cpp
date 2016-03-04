90f87a20cb1fd4a7d4a584171593a561d1c24786
Matevž Marš <matevz.mars@gmail.com>
2016-03-04 18:10:52 +0100

    few syntax errors corrected

--- LG3641BH.cpp
+++ LG3641BH.cpp
@@ -9,8 +9,11 @@ Released into the public domain
 
 #define numWait 700
 
-LG3641BH::LG3641BH(int pins, char type){
-	_pins = pins;
+LG3641BH::LG3641BH(int pins[12], char type){
+	for(int i=0;i<12;i++){
+		_pins[i] = pins[i];
+	}
+	
 	if(type == 'A'){
 		_state0 = LOW;
 		_state1 = HIGH;
@@ -25,16 +28,16 @@ void LG3641BH::begin(void){
 	for(int i=0;i<12;i++){
 		pinMode(_pins[i],OUTPUT);
 	}
-	for(i=0;i<8;i++){
+	for(int i=0;i<8;i++){
 		digitalWrite(_pins[i],_state0);
 	}
-	for(i=8;i<12;i++){
+	for(int i=8;i<12;i++){
 		digitalWrite(_pins[i],_state1);
 	}
 }
 
 int LG3641BH::number(int num){
-	if(n<0 || n>10) return 0; //n=10 corresponds dot segment
+	if(num<0 || num>10) return 0; //n=10 corresponds dot segment
 	
 	switch(num){
 		case 0:
@@ -242,12 +245,12 @@ int LG3641BH::writeNumber(int num, int pos){
 	}  
 }
 
-int writeLong(float num, int sec){
+int LG3641BH::writeLong(float num, int sec){
 	if(num<0 || num>9999) return 3;
 	
 	float temp=num;
 	
-	for(int j=0;j<microsecondsToClockCycles(1000)*sec;j++){
+	for(int j=0;j<microsecondsToClockCycles(1000000)*sec;j++){
 		num=temp;
 
 		if(0<=num && num<10){

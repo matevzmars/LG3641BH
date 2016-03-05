c40a55bedf14f73f67e580afd7275d6c5bb7cc3e
Matevž Marš <matevz.mars@gmail.com>
2016-03-05 12:51:10 +0100

    Changed my mind for creating separate functions for signed and unsigned numbers

--- LG3641BH.h
+++ LG3641BH.h
@@ -16,10 +16,8 @@ class LG3641BH
 		void begin(void);
 		int number(int num);
 		int writeNumber(int num, int pos);
-		int writeUFloat(float num, int sec);
-		int writeSFloat(float num, int sec);
-		int writeUInt(float num, int sec);
-		int writeSInt(float num, int sec);
+		int writeFloat(float num, int sec);
+		int writeInt(float num, int sec);
 	private:
 		int _pins[12];
 		int _state0;
